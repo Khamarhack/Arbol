3be4083ad5cfb0d53b4ca0ba7d9e94f737e1a655
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-26 11:36:23 -0500

    Update nodo.h

--- Arbol/nodo.h
+++ Arbol/nodo.h
@@ -1,3 +1,9 @@
+/*******************************************
+Fecha: 26 marzo 2025
+Autor: Santiago Camargo
+Tema: Taller Arboles
+Estructuras de datos
+******************************************/
 #ifndef __NODO__H__
 #define __NODO__H__
 
